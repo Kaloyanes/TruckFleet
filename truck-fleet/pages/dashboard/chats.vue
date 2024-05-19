811feac0f8552229520e8d81637a44df7fda2b91
Kaloyanes <kaloyangfx@gmail.com>
2024-05-19 18:08:58 +0300

    Started chat ui

--- truck-fleet/pages/dashboard/chats.vue
+++ truck-fleet/pages/dashboard/chats.vue
@@ -1,50 +1,31 @@
-<template>
-  <GMapMap :center="center" :zoom="10" map-type-id="terrain" style="width: 100vw; height: 20rem">
-    <GMapCluster :zoomOnClick="true">
-      <GMapMarker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true" :draggable="true"
-        @click="center = m.position" />
-    </GMapCluster>
-  </GMapMap>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      center: { lat: 51.093048, lng: 6.84212 },
-      markers: [
-        {
-          position: {
-            lat: 51.093048,
-            lng: 6.84212,
-          },
-        },
-        {
-          position: {
-            lat: 51.198429,
-            lng: 6.69529,
-          },
-        },
-        {
-          position: {
-            lat: 51.165218,
-            lng: 7.067116,
-          },
-        },
-        {
-          position: {
-            lat: 51.09256,
-            lng: 6.84074,
-          },
-        },
-      ],
-    };
-  },
-};
+<script lang="ts" setup>
+var titles = ["Drivers", "Speditors"]
 </script>
 
-<style>
-body {
-  margin: 0;
-}
-</style>
+<template>
+  <div class="flex w-full max-w-full my-2 mr-2 gap-3 items-center">
+
+    <ScrollArea class=" h-full flex-[0.3] rounded-2xl">
+      <div class="flex flex-col gap-3">
+
+        <div class="rounded-2xl w-full bg-[rgb(17,17,17)]" v-for="title in titles">
+          <h1 class="text-center py-5 text-xl font-semibold font-sans">{{ title }}</h1>
+
+          <div v-for="i in 10" :key="i" class=" h-20 mx-5">
+            {{ i }}
+          </div>
+        </div>
+      </div>
+    </ScrollArea>
+
+    <div class="flex-1 h-full rounded-2xl bg-[rgb(17,17,17)]">
+      <h1>Chat Window</h1>
+    </div>
+
+    <div class="flex-[0.3] h-full rounded-2xl bg-[rgb(17,17,17)]">
+      <h1>Chat Info</h1>
+    </div>
+
+
+  </div>
+</template>
