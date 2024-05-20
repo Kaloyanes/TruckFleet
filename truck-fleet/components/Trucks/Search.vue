400b54ac39d63c6374a9d86a6d7af7043b4ae47e
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2024-05-20 17:23:25 +0300

    Add search for license for trucks

---
+++ truck-fleet/components/Trucks/Search.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const searchInput = ref("");
+
+let trucksStore = useTrucksStore();
+// trucksStore.init();
+
+watch(searchInput, (value) => {
+  trucksStore.filterByLicensePlate(value);
+})
+</script>
+
+<template>
+  <Input placeholder="Search" class="flex-1 ml-4" v-model="searchInput" />
+</template>
+
+<style lang="scss"></style>
--- truck-fleet/pages/dashboard/trucks.vue
+++ truck-fleet/pages/dashboard/trucks.vue
@@ -9,29 +9,34 @@ useSeoMeta({
 
 
 
+const searchInput = ref("");
 
+const trucks = useTrucksStore();
+
+watch(searchInput, (value) => {
+  console.log(value);
+  trucks.filterByLicensePlate(value);
+})
 </script>
 
 <template>
   <div class="flex w-full max-w-full my-2 mr-2 gap-6">
-    <div class="flex flex-col flex-[0.2] gap-6 ">
+    <div class="flex flex-col flex-[0.2] flex-grow-0 gap-6 ">
 
       <div class="flex-[1] bg-[rgb(17,17,17)] rounded-xl  w-full relative overflow-scroll  flex flex-col h-full px-4">
 
-        <!-- TODO: Search field -->
-
         <div class="self-end p-3 mb-1 mx-2 sticky top-1 z-[50] flex items-center w-full gap-2">
-          <Input placeholder="Search" class="flex-1 ml-4" />
+          <LazyTrucksSearch />
 
           <Button @click="openTruckAddDialog">Add Truck</Button>
         </div>
 
         <div class="p-3 sticky top-12 z-[50] mx-2">
-          <TrucksFilterChips />
+          <LazyTrucksFilterChips />
         </div>
 
 
-        <TrucksList />
+        <LazyTrucksList />
 
 
       </div>
