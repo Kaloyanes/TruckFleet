a36f4e85b9d5f334398b04f99c9e36236e683196
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2024-08-16 01:55:48 +0300

    feat: Add new pages for Map, Drivers, and Chat

---
+++ truck-fleet-mobile/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+// Import your global CSS file
+import "../global.css";
+
+export default function RootLayout() {
+	return (
+		<Stack>
+			<Stack.Screen name="index" />
+		</Stack>
+	);
+}
