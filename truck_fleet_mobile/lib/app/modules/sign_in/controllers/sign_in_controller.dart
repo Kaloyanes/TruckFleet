14adb26cc9e52e4f00ff9483b1f08ba0a522530a
Kaloyanes <kaloyangfx@gmail.com>
2024-09-15 13:19:01 +0300

    Added toasts

--- truck_fleet_mobile/lib/app/modules/sign_in/controllers/sign_in_controller.dart
+++ truck_fleet_mobile/lib/app/modules/sign_in/controllers/sign_in_controller.dart
@@ -1,7 +1,11 @@
+import 'dart:ui';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:toastification/toastification.dart';
 import 'package:truck_fleet_mobile/app/modules/home/views/home_view.dart';
+import 'package:truck_fleet_mobile/app/services/toast_service.dart';
 
 class SignInController extends GetxController {
   //TODO: Implement SignInController
@@ -10,8 +14,37 @@ class SignInController extends GetxController {
   final passwordController = TextEditingController();
 
   void signIn() async {
+    showDialog(
+      context: Get.context!,
+      builder: (context) => BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+        child: const Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+
     await FirebaseAuth.instance
         .signInWithEmailAndPassword(email: emailController.text.trim(), password: passwordController.text.trim());
+
+    Navigator.of(Get.context!).pop();
+
+    // toastification.showCustom(
+    //   builder: (context, holder) => Container(
+    //     margin: const EdgeInsets.all(16),
+    //     decoration: BoxDecoration(
+    //       color: Theme.of(context).colorScheme.surface,
+    //       borderRadius: BorderRadius.circular(16),
+    //     ),
+    //     width: double.infinity,
+    //     height: 150,
+    //     child: const Text("Signed In"),
+    //   ),
+    //   autoCloseDuration: const Duration(seconds: 3),
+    //   dismissDirection: DismissDirection.up,
+    // );
+
+    ToastService.showToast("Signed In");
 
     Navigator.pushAndRemoveUntil(
       Get.context!,
