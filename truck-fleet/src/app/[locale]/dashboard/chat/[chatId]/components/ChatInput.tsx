095b4378f4e0bfd6d86bda7666b5ebe229569cb1
Kaloyanes <kaloyangfx@gmail.com>
2024-10-16 12:17:59 +0300

    Refactor chat input component and add send button
    - Refactored the chat input component in ChatInput.tsx to include a send button.
    - Imported the Button component from "@/components/ui/button".
    - Added the IconSend component from "@tabler/icons-react".
    - Used the useState and useEffect hooks to manage the message state and show/hide the send button.
    - Updated the Input component to bind the value to the message state and handle onChange events.
    - Added motion animations to the chat input component.
    
    Fix scroll area and add scroll bar in chat page
    
    - Updated the ScrollArea component in ChatSlug.tsx to have a fixed height.
    - Added the ScrollBar component from "@/components/ui/scroll-area".
    - Rendered a list of h1 elements with unique keys based on the chatId parameter.
    
    Refactor import statements in multiple files
    
    - Refactored the import statements in chat_controller.dart, home_controller.dart, and on_board_view.dart to improve code readability.
    
    Update packages and start chat UI
    
    - Updated the packages in the project.
    - Started working on the chat user interface.
    
    Refactor animation on get started screen
    
    - Refactored the animation on the get started screen to improve performance and user experience.

--- truck-fleet/src/app/[locale]/dashboard/chat/[chatId]/components/ChatInput.tsx
+++ truck-fleet/src/app/[locale]/dashboard/chat/[chatId]/components/ChatInput.tsx
@@ -1,11 +1,21 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { IconSend } from "@tabler/icons-react";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function ChatInput() {
+	const [message, setMessage] = useState("");
+	const [showSend, setShowSend] = useState(false);
+
+	useEffect(() => {
+		setShowSend(message.length > 0);
+	}, [message]);
+
 	return (
 		<motion.div
-			className="fixed right-0 bottom-0 left-0 m-2"
+			className="fixed right-0 bottom-0 left-0 m-2 flex items-center"
 			initial={{
 				scale: 0.2,
 				opacity: 0,
@@ -22,7 +32,17 @@ export default function ChatInput() {
 				},
 			}}
 		>
-			<Input placeholder="Type a message..." />
+			<Input
+				className="flex-1"
+				placeholder="Type a message..."
+				value={message}
+				onChange={(e) => setMessage(e.target.value)}
+			/>
+			<motion.div className="flex-[0.02]">
+				<Button className="ml-2" size={"icon"} disabled={!showSend}>
+					<IconSend />
+				</Button>
+			</motion.div>
 		</motion.div>
 	);
 }
--- truck-fleet/src/app/[locale]/dashboard/chat/[chatId]/page.tsx
+++ truck-fleet/src/app/[locale]/dashboard/chat/[chatId]/page.tsx
@@ -1,11 +1,12 @@
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 export default function ChatSlug({ params }: { params: { chatId: string } }) {
 	return (
 		<div className="relative flex flex-col overflow-y-scroll">
-			<ScrollArea className="flex-1 h-screen">
+			<ScrollArea className="h-screen">
+				<ScrollBar />
 				{new Array(250).fill(0).map((_, i) => (
-					<h1>My Post: {params.chatId}</h1>
+					<h1 key={i}>My Post: {params.chatId}</h1>
 				))}
 			</ScrollArea>
 		</div>
