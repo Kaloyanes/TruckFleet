96d20186579950e90abd608f2e631542464e3e30
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2024-10-26 15:10:48 +0300

    Refactor MapLayout component to remove unused imports and setRequestLocale function

--- truck-fleet/src/app/[locale]/dashboard/map/layout.tsx
+++ truck-fleet/src/app/[locale]/dashboard/map/layout.tsx
@@ -1,14 +1,10 @@
 "use client";
-import { use } from "react";
 import { APIProvider } from "@vis.gl/react-google-maps";
-import { setRequestLocale } from "next-intl/server";
 
 export default function MapLayout({
 	params: { locale },
 	children,
 }: { params: { locale: string }; children: React.ReactNode }) {
-	setRequestLocale(locale);
-
 	return (
 		<APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
 			{children}
