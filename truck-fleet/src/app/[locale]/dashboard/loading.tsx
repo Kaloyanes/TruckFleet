f22104b92c994312b4078963a9db3e067515c270
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2024-11-09 15:02:25 +0200

    Refactor loading components to use full height and width for better responsiveness; adjust overflow properties

--- truck-fleet/src/app/[locale]/dashboard/loading.tsx
+++ truck-fleet/src/app/[locale]/dashboard/loading.tsx
@@ -1,9 +1,8 @@
 import { Spinner } from "@/components/ui/loading-spinner";
 
 export default function Loading() {
-	// Or a custom loading skeleton component
 	return (
-		<div className="flex h-screen w-screen items-center justify-center overflow-hidden">
+		<div className="flex h-full w-full items-center justify-center overflow-y-hidden">
 			<Spinner />
 		</div>
 	);
--- truck-fleet/src/app/[locale]/dashboard/trucks/loading.tsx
+++ truck-fleet/src/app/[locale]/dashboard/trucks/loading.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "@/components/ui/loading-spinner";
 export default function Loading() {
 	// Or a custom loading skeleton component
 	return (
-		<div className="flex h-screen w-screen items-center justify-center overflow-hidden">
+		<div className="flex h-full w-full items-center justify-center overflow-hidden">
 			<Spinner />
 		</div>
 	);
