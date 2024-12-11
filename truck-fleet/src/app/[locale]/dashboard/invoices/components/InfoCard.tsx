497fe6e62937e20910e24590866482ef917bfece
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2024-12-11 21:07:27 +0200

    Refactor InfoCard and InvoiceTable components to utilize invoices store to show realtime statistics

--- truck-fleet/src/app/[locale]/dashboard/invoices/components/InfoCard.tsx
+++ truck-fleet/src/app/[locale]/dashboard/invoices/components/InfoCard.tsx
@@ -5,6 +5,7 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
+import { useInvoicesStore } from "@/stores/Invoices/InvoicesStore";
 import NumberFlow from "@number-flow/react";
 import { useLocale, useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
@@ -21,23 +22,24 @@ export default function InfoCard({
 	invoicesCount,
 }: InfoCardProps) {
 	const locale = useLocale();
-	const t = useTranslations("InvoicesPage");
-	const [count, setCount] = useState(amount);
+	const t = useTranslations("InvoiceList");
+	const { invoices: invoicesData } = useInvoicesStore();
+	const invoicesFiltered = invoicesData.filter(
+		(invoice) => invoice.status === description,
+	);
 	const [invoices, setInvoicesCount] = useState(0);
+	const [count, setCount] = useState(amount);
 
 	useEffect(() => {
-		const interval = setInterval(() => {
-			setCount(Math.round(Math.random() * 15000));
-		}, 1000);
-
-		return () => clearInterval(interval);
+		setCount(invoicesFiltered.reduce((acc, curr) => acc + curr.total, 0));
 	});
 
 	useEffect(() => {
-		setTimeout(() => {
-			setInvoicesCount(invoicesCount);
-		}, 200);
-	}, [invoicesCount]);
+		if (invoicesFiltered.length !== 0)
+			setTimeout(() => {
+				setInvoicesCount(invoicesFiltered.length);
+			}, 200);
+	}, [invoicesFiltered.length]);
 
 	return (
 		<Card className="min-h-40 flex-1">
@@ -55,8 +57,17 @@ export default function InfoCard({
 				</CardTitle>
 				<h2 className="font-semibold">{t(description as any)}</h2>
 				<CardDescription className="font-semibold text-xl/4xl">
-					<NumberFlow value={invoices} className="font-semibold text-xl/4xl" />{" "}
-					{t("invoices")}
+					{invoices !== 0 ? (
+						<>
+							<NumberFlow
+								value={invoices}
+								className="font-semibold text-xl/4xl"
+							/>{" "}
+						</>
+					) : (
+						<></>
+					)}
+					{t("invoices", { invoices: invoices })}
 				</CardDescription>
 			</CardHeader>
 		</Card>
